de9eada851814400e80ebe4778e24ec8207b1f79
haschish <haschish@gmail.com>
2020-05-20 15:07:23 +0500

    added simple navigation

--- app/src/main/java/com/example/volleyball/ui/main/MainFragment.java
+++ app/src/main/java/com/example/volleyball/ui/main/MainFragment.java
@@ -2,19 +2,22 @@ package com.example.volleyball.ui.main;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import android.media.SoundPool;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.volleyball.R;
 
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment implements View.OnClickListener {
 
     private MainViewModel mViewModel;
 
@@ -26,7 +29,10 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_fragment, container, false);
+        View v = inflater.inflate(R.layout.main_fragment, container, false);
+        Button b = v.findViewById(R.id.btnStart);
+        b.setOnClickListener(this);
+        return v;
     }
 
     @Override
@@ -36,4 +42,8 @@ public class MainFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
+    @Override
+    public void onClick(View v) {
+        Navigation.findNavController(v).navigate(R.id.mainToMatch);
+    }
 }
