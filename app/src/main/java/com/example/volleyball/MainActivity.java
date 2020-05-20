130e0c4b59fe7135e8d37566b3a4e381c88c190f
denn02 <denis.knyazev04@mail.ru>
2020-05-20 18:14:23 +0500

    first layout

--- app/src/main/java/com/example/volleyball/MainActivity.java
+++ app/src/main/java/com/example/volleyball/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.volleyball;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import com.example.volleyball.ui.main.MainFragment;
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 //        if (savedInstanceState == null) {
 //            getSupportFragmentManager().beginTransaction()
 //                    .replace(R.id.container, MainFragment.newInstance())
