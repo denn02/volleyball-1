df3ae4477f585fe0492bc6dab4d1d76d1a67d30d
haschish <haschish@gmail.com>
2020-05-26 02:19:23 +0500

    implemented fullscreen

--- app/src/main/java/com/example/volleyball/MainActivity.java
+++ app/src/main/java/com/example/volleyball/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.volleyball;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.volleyball.ui.main.MainFragment;
 
@@ -11,11 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         setContentView(R.layout.main_activity);
-//        if (savedInstanceState == null) {
-//            getSupportFragmentManager().beginTransaction()
-//                    .replace(R.id.container, MainFragment.newInstance())
-//                    .commitNow();
-//        }
     }
 }
